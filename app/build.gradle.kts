7092d3da5622cbf2c97dab5176db62274149ca4e
billyan <yan.billy.20@gmail.com>
2025-07-25 12:35:20 -0700

    feat: update compileSdk and targetSdk to API level 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,14 +9,14 @@ plugins {
 
 android {
     namespace = "com.minyu.moviesapp"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.minyu.moviesapp"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        targetSdk = 36
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
