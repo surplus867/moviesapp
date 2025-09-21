a67ebb7860cdc250b996a0af4a89e72a70c3ee02
billyan <yan.billy.20@gmail.com>
2025-09-20 20:49:53 -0700

    Remove API Key for security purpose

--- app/src/main/java/com/minyu/moviesapp/movieList/data/remote/MovieApi.kt
+++ app/src/main/java/com/minyu/moviesapp/movieList/data/remote/MovieApi.kt
@@ -58,6 +58,6 @@ interface MovieApi {
         // Base URL for movie images
         const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
         // API key for authentication (should be set securely)
-        const val API_KEY = "05663a72e30277317e18431232478d06"
+        const val API_KEY = ""
     }
 }
