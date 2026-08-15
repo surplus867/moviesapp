64a76c9cdbdd908c628f3e813e6ecd08c66e727f
billyan <yan.billy.20@gmail.com>
2026-08-14 18:50:48 -0700

    chore(app): bump versionCode for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.minyu.moviesapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.0"
+        versionCode = 11
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
